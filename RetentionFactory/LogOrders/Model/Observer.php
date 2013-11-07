d2f08746e789400d54f2d78298d10ae6c80f3559
Tim Cull <trcull@thedwick.com>
2013-11-07 10:49:41 -0800

    added lots of error checking

--- RetentionFactory/LogOrders/Model/Observer.php
+++ RetentionFactory/LogOrders/Model/Observer.php
@@ -22,33 +22,73 @@ class RetentionFactory_LogOrders_Model_Observer
     public function logOrder(Varien_Event_Observer $observer)
     {
     	Mage::log("RetentionFactory Observer Got An Order");
-        // Retrieve the product being updated from the event observer
-        $order = $observer->getEvent()->getOrder();
-    	Mage::log("order id is {$order->getId()} and state {$order->getState()} and status {$order->getStatus()} and real id {$order->getRealOrderId()}");
-		$customer = $order->getCustomer();
-		$to_post = array('order_lines'=>array(),
-					'customer'=>array('email'=>$customer->getEmail(),
-									'org_id'=>$customer->getId(),
-									'org_created_at'=>$customer->getCreatedAt()),
-					'org_created_at'=>$order->getCreatedAt(),
-					'org_id'=>$order->getId()
-					);
-		//if($order->getState() == Mage_Sales_Model_Order::STATE_COMPLETE){
-			$items = $order->getAllVisibleItems();
-		    foreach($items as $item) {
-		        $product = $item->getProduct();
-		    	$line_to_post = array(
-		    				'quantity'=>$item->getQtyOrdered(),
-		    				'price'=>$item->getPrice(),
-		    				'product'=>array('org_id'=>$product->getId(),
-										'name'=>$product->getName(),
-										'org_created_at'=>$product->getCreatedAt()));
-				$to_post['order_lines'][] = $line_to_post;
-		    }
-   		//}
-		
-		$json = json_encode($to_post);
-		Mage::log("Posting JSON: {$json}");
-		
+		try {
+	        // Retrieve the product being updated from the event observer
+	        $order = $observer->getEvent()->getOrder();
+			if (isset($order)){
+		    	Mage::log("order id is {$order->getId()} and state {$order->getState()} and status {$order->getStatus()} and real id {$order->getRealOrderId()}");
+				//I'd really like to look for STATE_COMPLETE, but when that event is fired then $order->getCustomer() is null.  So I have no choice but to use STATE_NEW
+				if($order->getState() == Mage_Sales_Model_Order::STATE_NEW){
+					$customer = $order->getCustomer();
+					if (isset($customer)){
+						if ($customer->getId()){
+							$customer_id = $customer->getId();
+							if ($customer->getEmail()){
+								$customer_email = $customer->getEmail();
+							} else {
+						        foreach ($order->getAddressesCollection() as $address) {
+						            if ($address->getEmail()) {
+										$customer_email = $address->getEmail();
+										break;
+						            }
+						        }
+							}
+							$customer_created_at = $customer->getCreatedAt();
+						} else {
+					        foreach ($order->getAddressesCollection() as $address) {
+					            if ($address->getEmail()) {
+									$customer_email = $address->getEmail();
+									break;
+					            }
+					        }
+							$customer_id = 'guest-'.$customer_email;
+							$customer_created_at = null;
+						}
+						$to_post = array('order_lines'=>array(),
+									'customer'=>array('email'=>$customer_email,
+													'org_id'=>$customer_id,
+													'org_created_at'=>$customer_created_at),
+									'org_created_at'=>$order->getCreatedAt(),
+									'org_id'=>$order->getRealOrderId()
+									);
+						$items = $order->getAllVisibleItems();
+					    foreach($items as $item) {
+					        $product = $item->getProduct();
+							if (isset($product)){
+						    	$line_to_post = array(
+						    				'quantity'=>$item->getQtyOrdered(),
+						    				'price'=>$item->getPrice(),
+						    				'product'=>array('org_id'=>($product->getSku() ? $product->getSku() : $product->getId()),
+														'name'=>$product->getName(),
+														'org_created_at'=>$product->getCreatedAt()));
+								$to_post['order_lines'][] = $line_to_post;
+							} else {
+								Mage::log("product is null, ignoring");
+							}//isset($product)
+					    }
+						$json = json_encode($to_post);
+						Mage::log("Posting JSON: {$json}");
+					} else {
+						Mage::log("customer is null, ignoring");
+					}//isset($customer)
+				} else {
+					Mage::log("state is not ".Mage_Sales_Model_Order::STATE_NEW.", ignoring");
+				}//status complete
+			} else {
+				Mage::log("order is null, ignoring");
+			}//isset($order)
+		} catch (Exception $e) {
+			Mage::log("RetentionFactory Observer Caught An Exception: ".$e->getMessage());
+		}		
     }
 }
